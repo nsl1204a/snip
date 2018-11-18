53ad3ebe023b32734f57efb9d4e015a62f201a4e
Manuel <manuel.calvo@elespanol.com>
2018-11-18 19:50:29 +0100

    uso de commit con amend

--- php/for.php
+++ php/for.php
@@ -1,1 +1,3 @@
-<php esto seria un for para php pero no se como se hace asique se queda asi
+
+<<php esto seria un for para php pero no se como se hace asique se queda asi
+<php esto es una modificacion del for php 
